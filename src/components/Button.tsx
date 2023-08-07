aa913d1f3b5f94fa1e5fbcb70f748845076c2e16
Nikhil Shrestha <nikhilshrestha0@gmail.com>
2023-08-07 14:02:13 -0700

    completed hero

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ type ButtonProps = {
 const Button = ({ name, className }: ButtonProps) => {
   return (
     <button
-      className={`bg-gradient-to-r from-purple-500 to-pink-500 px-4 py-1  rounded ${className} cursor-pointer hover:opacity-80`}
+      className={`bg-gradient-to-r from-purple-500 to-pink-500 px-4 py-1  rounded w-full cursor-pointer  hover:opacity-80 ${className}`}
     >
       {name}
     </button>
---
+++ src/components/Hero.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Button from "./Button";
+import Container from "./Container";
+import Image from "next/image";
+import wolfMobile from "../../public/wolf-mobile.png";
+import lightHouse from "../../public/light-house.png";
+
+const Hero = () => {
+  return (
+    <div className="pt-20 text-center bg-gradient-to-b from-black to-indigo-500">
+      <Container>
+        <h1 className="text-center text-3xl font-semibold my-8 leading-relaxed">
+          The Fastest Email Experience Ever Made
+        </h1>
+        <p className="text-lg leading-loose font-medium pt-8 pb-10">
+          Blazingly fast, delightful and stress free. <br /> Get 3 hours back
+          every single week
+        </p>
+        <Button name="Get Started" className="py-2.5 mb-20 max-w-sm" />
+
+        <div className="relative m-auto max-w-[26rem]">
+          <Image
+            src={wolfMobile}
+            width={380}
+            height={22}
+            alt="pic"
+            className="m-auto relative right-5 mw-100"
+          />
+          <Image
+            className="absolute bottom-0 right-0 mw-100"
+            src={lightHouse}
+            width={100}
+            height={22}
+            alt="pic"
+          />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
